c9ab2fd186c123608c006716d9273638524b5af7
KALVIUM23 <rockstarsagarkv206@gmail.com>
2025-02-22 16:42:06 +0530

    done

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const { resolve } = require('path');
+require("dotenv").config();
+const mongoose =require("mongoose");
 
 const app = express();
 const port = 3010;
+app.use(express.json())
+
+//mongoose conection 
+
+mongoose.connect(process.env.mongo_url)
+.then(()=>(
+  console.log("connected to database")
+))
+.catch((err)=>(
+  console.log("error connecting to database",err)
+))
 
 app.use(express.static('static'));
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
